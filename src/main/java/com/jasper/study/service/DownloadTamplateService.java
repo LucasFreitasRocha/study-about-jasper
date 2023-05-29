8a40bbeab36a3a447ce66d25dfe3248702b2f5b2
Lucas Freitas da Rocha <rochadefreitaslucas@gmail.com>
2023-05-29 17:33:33 -0300

    get template from github repository

---
+++ src/main/java/com/jasper/study/service/DownloadTamplateService.java
@@ -0,0 +1,24 @@
+package com.jasper.study.service;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.net.URL;
+
+@Service
+public class DownloadTamplateService {
+    public File downlaodTempalte(String templateName){
+        try {
+            URL url = new URL("https://raw.githubusercontent.com/LucasFreitasRocha/templates-jasper-study/master/" +templateName);
+            File file = ResourceUtils.getFile("classpath:templates/defaultTemplate.jrxml");;
+            FileUtils.copyURLToFile(url, file);
+            return file;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
--- src/main/java/com/jasper/study/service/GenerateDocumentsService.java
+++ src/main/java/com/jasper/study/service/GenerateDocumentsService.java
@@ -6,14 +6,12 @@ import com.jasper.study.dto.in.RequestDTO;
 import com.jasper.study.dto.out.CustomMultipartFile;
 import com.jasper.study.dto.out.ResponseDTO;
 import jakarta.servlet.http.HttpServletResponse;
-import jdk.jfr.ContentType;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
@@ -21,39 +19,47 @@ import java.util.*;
 @Service
 public class GenerateDocumentsService {
 
+    @Autowired
+    private DownloadTamplateService downloadTamplateService;
 
     public ResponseDTO genereteBase64(RequestDTO requestDTO) {
-        ResponseDTO responseDTO = new ResponseDTO();
-        try {
-            responseDTO.setFileBase64(Base64.getEncoder().encodeToString((generete(requestDTO))));
-           // responseDTO.setFile(new CustomMultipartFile(arquivoPdf, "teste", "pdf"));
-            return responseDTO;
-        } catch (Exception e) {
-            e.printStackTrace();
+        File file = downloadTamplateService.downlaodTempalte("poc.jrxml");
+        if (!Objects.isNull(file)) {
+            ResponseDTO responseDTO = new ResponseDTO();
+            try {
+                responseDTO.setFileBase64(Base64.getEncoder().encodeToString((generete(requestDTO, file.getAbsolutePath()))));
+                return responseDTO;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
         return null;
     }
 
-    public void prepereDonwload(HttpServletResponse response, RequestDTO requestDTO){
-        CustomMultipartFile customMultipartFile = new CustomMultipartFile(generete(requestDTO), "teste", "pdf");
-        try {
-            response.setHeader("Content-disposition", "attachment; filename="
-                    + customMultipartFile.getOriginalFilename());
-            response.setHeader("Cache-Control", "max-age=86400, public");
-            response.setContentType("application/pdf");
-            IOUtils.copy(customMultipartFile.getInputStream(), response.getOutputStream() );
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+    public void prepereDonwload(HttpServletResponse response, RequestDTO requestDTO) {
+        File file = downloadTamplateService.downlaodTempalte("poc.jrxml");
+        if (!Objects.isNull(file)) {
+            CustomMultipartFile customMultipartFile = new CustomMultipartFile(generete(requestDTO, file.getAbsolutePath()), "teste", "pdf");
+            try {
+                response.setHeader("Content-disposition", "attachment; filename="
+                        + customMultipartFile.getOriginalFilename());
+                response.setHeader("Cache-Control", "max-age=86400, public");
+                response.setContentType("application/pdf");
+                IOUtils.copy(customMultipartFile.getInputStream(), response.getOutputStream());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
-    private byte[] generete(RequestDTO requestDTO){
+    private byte[] generete(RequestDTO requestDTO, String pathFile) {
         ResponseDTO responseDTO = new ResponseDTO();
         try {
-            File file = ResourceUtils.getFile("classpath:templates/poc.jrxml");
+
             Map<String, Object> mapVariables = new HashMap<>();
             requestDTO.getFields().forEach(field -> buildMapVariables(field, mapVariables));
-            JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+            JasperReport jasperReport = JasperCompileManager.compileReport(pathFile);
             JasperPrint print = JasperFillManager.fillReport(jasperReport, mapVariables,
                     new JRBeanCollectionDataSource(Collections.singleton(mapVariables)));
             return JasperExportManager.exportReportToPdf(print);
